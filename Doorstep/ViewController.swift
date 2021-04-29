2666312dd6194ec47d265736e711f6aada65b1e0
Karan Gajjar <karangajjar.lp@gmail.com>
2021-04-29 05:18:02 -0400

    first commit

--- Doorstep/ViewController.swift
+++ Doorstep/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var loginView: UIView!
+    
+    @IBOutlet weak var registerView: UIView!
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex
+            {
+        
+//            case 0:
+//                textLabel.text = "First Segment Selected"
+//            case 1:
+//                textLabel.text = "Second Segment Selected"
+            default:
+                break
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
